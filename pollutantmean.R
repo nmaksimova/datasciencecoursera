e0e35993fa432e92f8edaffe21611cf36866867b
nmaksimova <nataliia.maksimova@gmail.com>
2016-08-01 12:14:06 -0400

    Add a function pollutantmean.R

---
+++ pollutantmean.R
@@ -0,0 +1,30 @@
+setwd("C:/Users/namaksi/Documents/Data_Science/WEEK2")
+getwd()
+list.files()
+
+
+pollutantmean <- function(directory, pollutant, id = 1:5) {
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+        
+        ## 'pollutant' is a character vector of length 1 indicating
+        ## the name of the pollutant for which we will calculate the
+        ## mean; either "sulfate" or "nitrate".
+        
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+        
+        ## Return the mean of the pollutant across all monitors list
+        ## in the 'id' vector (ignoring NA values)
+        
+        data = numeric()
+        for (i in id) {
+                
+                newRead = read.csv(paste(directory, "/", formatC(i, width = 3, flag = "0"), 
+                                         ".csv", sep = ""))
+                
+                data = c(data, newRead[[pollutant]])
+        }
+        return(mean(data, na.rm = TRUE))
+}
+pollutantmean("specdata", "nitrate", 3)
