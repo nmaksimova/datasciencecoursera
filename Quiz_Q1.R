6f477aa39004317f650f0f00e6c9fe3bcca6f7a4
nmaksimova <nataliia.maksimova@gmail.com>
2016-09-19 14:37:17 -0400

    Updated the third question

--- Quiz_Q1.R
+++ Quiz_Q1.R
@@ -24,10 +24,29 @@ quantile(data2, probs = c(0.3, 0.8))
 if(!file.exists("./q3")) {dir.create("./q3")}
 fileUrl1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
 fileUrl2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
-download.file(fileUrl1, destfile = "./q3/2FGDP.csv")
-download.file(fileUrl2, destfile = "./q3/FEDSTATS_Country.csv")
+download.file(fileUrl1, destfile = "./q3/2FGDP.csv", mode='wb')
+download.file(fileUrl2, destfile = "./q3/FEDSTATS_Country.csv", mode='wb')
 GDP <- read.csv("./q3/2FGDP.csv")
 edu <- read.csv("./q3/FEDSTATS_Country.csv")
 
-names(head(GDP,2))
-head(edu,2)
+#names(head(GDP,2))
+#head(edu,10)
+#head(GDP,10)
+#names(head(edu,2))
+
+#install.packages("data.table")
+#library(data.table)
+
+install.packages("plyr")
+library(plyr)
+
+
+gdp_read = fread("./q3/2FGDP.csv", skip = 4, nrows = 190, select = c(1, 2, 4, 5), col.names = c("CountryCode", "Rank", "Economy", "Total"))
+edu_read = fread("./q3/FEDSTATS_Country.csv")
+
+merge_tabs = merge(gdp_read, edu_read, by = "CountryCode")
+
+nrow(merge_tabs)
+
+arrange(merge_tabs, desc(Rank))
+arrange(merge_tabs, desc(Rank))[13, Economy]
